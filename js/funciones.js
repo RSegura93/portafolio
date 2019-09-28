45956fe80a95c5aa804fb5c0a731189f9beedb46
a20111731 <a20111371@pucp.pe>
2019-09-27 23:39:45 -0500

    cleaning funciones.js code

--- js/funciones.js
+++ js/funciones.js
@@ -1,47 +1,56 @@
-window.onload = function(){
+"use strict";
+var addSelectors = window.addSelectors;
+var $ = window.$;
+var addTagstoProjects = window.addTagstoProjects;
+var updateShowFormBy = window.updateShowFormBy;
+var handlerEventOrderProjects = window.handlerEventOrderProjects;
+var handlerEventPresentationFormat = window.handlerEventPresentationFormat;
+/* exported capitalizeFirstLetter*/
+
+window.onload = function() {
 	// start selectPicker from bootstrap
 	addSelectors();
 	$('.selectpicker').selectpicker();
-	
+
 	// update data from projects
 	addTagstoProjects();
-	updateShowFormBy ( );
+	updateShowFormBy();
 
 	// handler events 
 	handlerTopMenu();
 	handlerEventOrderProjects();
 	handlerEventPresentationFormat();
 	handlerContactList();
-}
+};
 
 function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+	return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-function linkToContact( type ){
-	if( type == "mail" ) {
+function linkToContact(type) {
+	if (type == "mail") {
 		window.open('mailto:renzosegurapm@gmail.com');
-	}else if( type == "github" ) {
+	} else if (type == "github") {
 		window.open('https://github.com/rsegura93');
-	}else if( type == "home" ) {
-		location.href=location.href;
+	} else if (type == "home") {
+		location.href = location.href;
 	}
 }
 
-function handlerContactList(){
-	$(".footer-menu ul>li").click(function(){
+function handlerContactList() {
+	$(".footer-menu ul>li").click(function() {
 		var type = $(this).find("label").
-					attr("class").replace("icon ", "");
-		linkToContact ( type );
-	})
+		attr("class").replace("icon ", "");
+		linkToContact(type);
+	});
 }
 
-function handlerTopMenu(){
-	$(".top-menu .menu").hover( function(){
+function handlerTopMenu() {
+	$(".top-menu .menu").hover(function() {
 		var desplegable = $(this).find("ul");
-		if( desplegable.hasClass("hidden") ){
-			desplegable.removeClass("hidden")
-		}else{
+		if (desplegable.hasClass("hidden")) {
+			desplegable.removeClass("hidden");
+		} else {
 			$(this).find("ul").addClass("hidden");
 		}
 	});
